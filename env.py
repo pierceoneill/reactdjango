2c0a7b02c1e04f12aec6ed0542072e43eb466bcb
Pierce O'Neill <pierceoneill@Pierces-MacBook-Pro.local>
2021-10-10 16:12:09 +0100

    secured settings for aws bucket

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAULJI3YBH3H5TO55C")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY_ID", "YowJD6HQEBfKgZ2k8K4e1hqW5h0jajuFdmi5SuQt")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAULJI3YBH7R7CZNHI")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY_ID", "WpyHX4fQqQhK+7MlFsdrA4gcqHeksUw0MWAsIep8")
 os.environ.setdefault("AWS_STORAGE_BUCKET_NAME", "reactdjango")
 os.environ.setdefault("DB_PASS", "Mountain01!")
