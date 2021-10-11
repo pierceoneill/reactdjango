624ddaa67020e0e7bd4d3bc37db7630ec81de2dc
Pierce O'Neill <37594104+pierceoneill@users.noreply.github.com>
2021-10-11 10:42:29 +0100

    Delete env.py
    removing keys

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAULJI3YBH7R7CZNHI")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY_ID", "WpyHX4fQqQhK+7MlFsdrA4gcqHeksUw0MWAsIep8")
-os.environ.setdefault("AWS_STORAGE_BUCKET_NAME", "reactdjango")
-os.environ.setdefault("DB_PASS", "Mountain01!")
